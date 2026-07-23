abf58d6ecd4deb618b54821ec7d313c12d7bb470
mohamedismail2 <mohameismail20@gmail.com>
2026-07-23 15:31:40 +0300

    done

--- frontend/src/assets/components/Cart.jsx
+++ frontend/src/assets/components/Cart.jsx
@@ -1,27 +1,19 @@
 import { ShoppingCart } from "lucide-react";
 import { useGetAllItemCartQuery } from "../slices/cartApiSlice";
-// import { useNavigate } from "react-router-dom";
-// import { useSelector } from "react-redux";
 
 function Cart() {
+
   // qty in cart
   const { data: cart = [] } = useGetAllItemCartQuery();
-  // const { cartItems } = useSelector((state) => state.cart);
-  console.log("Cart=", cart);
+
   console.log("isArray=", Array.isArray(cart));
 
-  // const totalItems = cart.reduce((sum, item) => sum + item.qty, 0);
   let totalItems = 0;
 
   if (Array.isArray(cart)) {
     totalItems = cart.reduce((sum, item) => sum + item.qty, 0);
   }
 
-  // const navigate = useNavigate();
-
-  // const handleCart = () => {
-  //   navigate("/carts");
-  // };
 
   return (
     <div className="relative">
